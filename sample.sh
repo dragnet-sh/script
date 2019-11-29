02a5947a0ed3e7c9f93c4ac8acba0c42f2399dd4
dotsh <30569215+dragnet-sh@users.noreply.github.com>
2019-11-29 11:19:12 +0545

    extracting annotation

--- sample.sh
+++ sample.sh
@@ -2,3 +2,6 @@
 # ToDo - Find a better one liner to do this !!
 
 for f in ./*; do mv "$f" "${f//-Table 1/}"; done
+
+# Extracting specific words from Annotations
+grep "Highlight.*\:\s[a-zA-Z]\+:\?$" ./ETS_Big_Verbal.txt | cut -d ':' -f 2 | awk '{print tolower($0);}'
