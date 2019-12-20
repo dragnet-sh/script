d57ccff0c0bdfd9ac2cd258bd007524357ef02cb
dotsh <30569215+dragnet-sh@users.noreply.github.com>
2019-12-20 21:18:03 +0545

    extract the latest task title from doing

--- sample.sh
+++ sample.sh
@@ -5,3 +5,6 @@ for f in ./*; do mv "$f" "${f//-Table 1/}"; done
 
 # Extracting specific words from Annotations
 grep "Highlight.*\:\s[a-zA-Z]\+:\?$" ./ETS_Big_Verbal.txt | cut -d ':' -f 2 | awk '{print tolower($0);}'
+
+# Extracting the task title from doing
+doing recent 1 | egrep -o ">(.*?)@" | sed -r 's/>\s|\s@//g'
