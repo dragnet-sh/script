846d4d16b874a960f246e91c37644dd03701468b
dotsh <30569215+dragnet-sh@users.noreply.github.com>
2019-12-20 21:26:59 +0545

    use xarg to give the output to doing now

--- sample.sh
+++ sample.sh
@@ -6,5 +6,5 @@ for f in ./*; do mv "$f" "${f//-Table 1/}"; done
 # Extracting specific words from Annotations
 grep "Highlight.*\:\s[a-zA-Z]\+:\?$" ./ETS_Big_Verbal.txt | cut -d ':' -f 2 | awk '{print tolower($0);}'
 
-# Extracting the task title from doing
-doing recent 1 | egrep -o ">(.*?)@" | sed -r 's/>\s|\s@//g'
+# Extracting the task title from doing and creating a new entry
+doing recent 1 | egrep -o ">(.*?)@" | sed -r 's/>\s|\s@//g' | xargs doing now
